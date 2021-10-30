cd23402b3362b00b7cfafa4af662b5378dcf61a7
karthiksripad18 <karthiksripad18@gmail.com>
2021-10-30 14:36:02 +0530

    sidebar component design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container } from 'react-bootstrap';
 
 import Header from './components/header/Header';
@@ -8,11 +8,13 @@ import HomeScreen from './screens/homeScreen/HomeScreen';
 import './_app.scss';
 
 const App = () => {
+    const [sidebar, toggleSidebar] = useState(false);
+    const handleToggleSidebar = () => toggleSidebar(value => !value);
     return (
         <>
-            <Header />
+            <Header handleToggleSidebar={handleToggleSidebar} />
             <div className="border app__container border-info">
-                <Sidebar />
+                <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
                 <Container fluid className="border app__main border-warning">
                     <HomeScreen />
                 </Container>
