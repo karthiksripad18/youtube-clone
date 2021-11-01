dab7183164612aaa0a1379b946dc8eb6a5a54b86
karthiksripad18 <karthiksripad18@gmail.com>
2021-11-01 15:05:00 +0530

    firebase set up

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD7QOGCobpTNIiQCMaUto6VF6_QW4wAq4s",
+    authDomain: "yt-clone-karthik.firebaseapp.com",
+    projectId: "yt-clone-karthik",
+    storageBucket: "yt-clone-karthik.appspot.com",
+    messagingSenderId: "1058577593948",
+    appId: "1:1058577593948:web:5485e60c85868f25fb1899"
+};
+
+firebase.initializeApp(firebaseConfig);
